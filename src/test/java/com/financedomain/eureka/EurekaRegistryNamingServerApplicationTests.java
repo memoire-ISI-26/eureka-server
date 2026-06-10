42534d915e669cff32e27733485c6dab083a7ba5
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-06-10 22:54:33 +0000

    Initial commit

---
+++ src/test/java/com/financedomain/eureka/EurekaRegistryNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financedomain.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
